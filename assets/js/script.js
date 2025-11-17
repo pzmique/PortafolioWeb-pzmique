b0acdbae06f8f5f333bacc49353faf0d61336440
pzmique <152024423+pzmique@users.noreply.github.com>
2025-11-17 11:21:22 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const texto = "TP Integrador 2";
+const textoEscrito = document.getElementById("name");
+const subtitle = document.getElementById("subtitle");
+
+subtitle.textContent = "Paz Miqueas </>";
+
+let i = 0;
+
+function typeWriter() {
+    if (i < texto.length) {
+        textoEscrito.textContent += texto[i];
+        i++;
+        setTimeout(typeWriter, 100);
+    }
+}
+
+typeWriter();
